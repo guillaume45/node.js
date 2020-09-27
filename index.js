0b2ae71ea33dea4d03d23ad46dd2b8e928e57641
guillaume45 <71097397+guillaume45@users.noreply.github.com>
2020-09-27 19:35:29 +0200

    Add files via upload
    ajout du fichier index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let name = `Guillaume Couturier`;
+let age = 36;
+let passions = [`resue`, `travel`, `movies`];
+console.log(name, age, passions);
+
+const myInformation = require('./information.js);
+myInformation();
