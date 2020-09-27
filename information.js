87aec0b625a41b034cf39f1e158485fe079e1f49
guillaume45 <71097397+guillaume45@users.noreply.github.com>
2020-09-27 19:36:01 +0200

    Add files via upload
    ajout du fichier information.js

---
+++ information.js
@@ -0,0 +1,9 @@
+let name = `Guillaume Couturier`;
+let age = 36;
+let passions = [`resue`, `travel`, `movies`];
+
+const myInfos = () => {
+    console.log(`Hi my name is ${name}, I'm ${age} and my passions are ${passions}`);
+};
+
+module.exports = myInfos;
